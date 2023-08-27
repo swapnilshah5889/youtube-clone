d2b03e90868e97fe66ead35b29f9d12217f24c51
Swapnil Shah <swapnilshah5889@gmail.com>
2023-08-27 10:43:56 -0400

    Added video card

---
+++ src/components/VideoCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const VideoCard = (props:any) => {
+  return (
+    <div>
+        <img src="/photo.png" className='rounded-xl' alt='img'></img>
+        <div className='grid grid-cols-12 pt-2'>
+            
+            <div className='col-span-1'>
+                <img className='rounded-full w-12 h-12' 
+                src="/photo.png" alt='img'></img>
+            </div>
+
+            <div className='col-span-11 pl-1'>
+                <div className='col-span-11'>
+                    {props.title}
+                </div>
+                <div className='col-span-11 text-gray-400 text-base'>
+                    {props.author}
+                </div>
+                <div className='col-span-11 text-gray-400 text-base'>
+                    {props.views} | {props.timestamp}
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default VideoCard;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import VideoCard from '@/components/VideoCard'
 
 const inter = Inter({ subsets: ['latin'] })
 
+const VIDEOS = [{}]
+
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div>
+      <VideoCard 
+      title="Volvo XC40 Recharge"
+      author="Autocar India"
+      timestamp="13 days ago"
+      views="40k"
+      />
+    </div>
   )
 }
